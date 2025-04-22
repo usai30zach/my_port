f8b726513370408f9ef01afc929ff87013dc6fb5
usai30zach <sai@newworld.local>
2025-04-22 12:00:29 -0700

    update experience and fixed bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Portfolio from '/App.jsx';
+import Portfolio from './App.jsx';
 import './index.css'; // Only if you have Tailwind via PostCSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
